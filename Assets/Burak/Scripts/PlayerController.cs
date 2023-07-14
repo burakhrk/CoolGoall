21f11179ab83b31eb15aa98bbc9acf77fbd11b97
Xenolia <burak@xenolia.com>
2023-07-14 11:39:40 +0300

    güncel

--- Assets/Burak/Scripts/PlayerController.cs
+++ Assets/Burak/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ using UnityEngine.InputSystem;
     Vector2 lastPos;
     Vector3 bezierStartPos;
     [SerializeField] GameObject bezierMove;
+    [SerializeField] GameObject bezierGoal;
     [SerializeField] float sensivity=1f;
+    [SerializeField] float bound=3f;
     private void Awake()
     {
        bezierStartPos= bezierMove.transform.position;
@@ -38,11 +40,24 @@ using UnityEngine.InputSystem;
     void MoveBezier()
     { 
 
-        bezierMove.transform.position += (Vector3)move * sensivity * sensivity;
+        bezierMove.transform.position += (Vector3)move * sensivity ;
 
         var a = bezierMove.transform.position.y ;
         if (a < 0)
             bezierMove.transform.position = new Vector3(bezierMove.transform.position.x,0,bezierMove.transform.position.z);
 
+        var b = bezierMove.transform.position.x;
+
+        if (Mathf.Abs(b)>3)
+        {
+            bezierMove.transform.position = new Vector3(bezierMove.transform.position.x, 0, bezierMove.transform.position.z);
+
+        }
+        MoveGoal();
+
+    }
+    void MoveGoal()
+    {
+        bezierGoal.transform.position += (Vector3)move;
     }
 }
